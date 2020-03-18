c71eb65fa832d24aee1d24370f8720cedfde697a
jahed <jahed.w.dev@gmail.com>
2020-03-18 09:12:45 +0600

    modified

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
+const postionsElement = document.getElementsByClassName("postion");
 const hoverElement = document.getElementsByClassName("hover");
 const toggleElement = document.getElementsByClassName("toggle");
 
-for (let index = 0; index < hoverElement.length; index++) {
+for (let index = 0; index < postionsElement.length; index++) {
   const singleHover = hoverElement[index];
   const singleToggle = toggleElement[index];
+  const postionElement = postionsElement[index];
 
-  singleHover.addEventListener("mouseover", event => {
+  postionElement.addEventListener("mouseover", event => {
     singleToggle.className = "displayBlock";
     if (event.screenX > 980) {
       singleToggle.style.cssText = "top: 0; left: -100%";
     }
-    // console.log(event.screenX);
   });
-  singleHover.addEventListener("mouseout", event => {
+  postionElement.addEventListener("mouseout", event => {
     singleToggle.className = "displayNone";
   });
 }
-
-// hoverElement.addEventListener("on", e => {
-//   const x = e.screenX;
-//   console.log(x);
-// });
